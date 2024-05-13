4e24fc3a2d6dffc861f2fabb783ff9a430503ebc
jengros <92702203+jengros@users.noreply.github.com>
2024-05-14 07:33:31 +0900

    Update rs485.py

--- kocomRS485/rs485.py
+++ kocomRS485/rs485.py
@@ -641,7 +641,9 @@ class Kocom(rs485):
             subscribe_list.append((ha_topic, 0))
             subscribe_list.append((ha_payload['cmd_t'], 0))
             #subscribe_list.append((ha_payload['stat_t'], 0))
-            subscribe_list.append((ha_payload['spd_cmd_t'], 0))
+            
+            #subscribe_list.append((ha_payload['spd_cmd_t'], 0))
+            subscribe_list.append((ha_payload['pr_mode_cmd_t'], 0)) 
             if remove:
                 publish_list.append({ha_topic : ''})
             else:
@@ -1207,7 +1209,9 @@ class Grex:
             }
         subscribe_list.append((ha_topic, 0))
         subscribe_list.append((ha_payload['cmd_t'], 0))
-        subscribe_list.append((ha_payload['spd_cmd_t'], 0))
+        #subscribe_list.append((ha_payload['spd_cmd_t'], 0))
+        subscribe_list.append((ha_payload['pr_mode_cmd_t'], 0)) 
+        
         #subscribe_list.append((ha_payload['stat_t'], 0))
         publish_list.append({ha_topic : json.dumps(ha_payload)})
 
